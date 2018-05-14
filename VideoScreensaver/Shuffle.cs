f96e070be511354b01bfa8391bf33eb2d2034137
Ricardo Ferreira <ricardo.ferreira@rackspace.co.uk>
2018-05-14 22:21:17 +0100

    Initial commit (#1)

---
+++ VideoScreensaver/Shuffle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VideoScreensaver
+{
+    public static class Shuffle
+    {
+        public static void ShufVideo<T>(this IList<T> list, Random rnd)
+        {            
+            var listCount = list.Count;
+
+            for (var index = 0; index < listCount; index++)
+            {
+                var nextRnd = rnd.Next(index, listCount);
+
+                var temp = list[index];
+                list[index] = list[nextRnd];
+                list[nextRnd] = temp;
+            }
+        }
+    }
+}
